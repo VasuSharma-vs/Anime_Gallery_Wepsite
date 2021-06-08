4bccc41520595c9930bb9b84770e74be2f281a73
vasu sharma <vasusharma244@gmail.com>
2021-06-08 18:55:13 +0530

    heroku and mongoose update wip

--- app.js
+++ app.js
@@ -4,10 +4,24 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import ejs from 'ejs'
 import mongoose from 'mongoose';
 import Env from 'dotenv';
+import { futimesSync } from 'fs';
 
 // imported modules /////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+// setting up envirment variables ///////////////////////////////////////////////////////////////////////////////
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const env = Env.config();
+const app = express();
+
+// setted up envirment variables ///////////////////////////////////////////////////////////////////////////////
 
 
 
@@ -23,64 +37,79 @@ app.use(express.static(__dirname+"/public"));
 
 
 
-// setting up envirment variables ///////////////////////////////////////////////////////////////////////////////
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const env = Env.config();
-const app = express();
-
-// setting up envirment variables ///////////////////////////////////////////////////////////////////////////////
-
-
-
-
 // Mongoose Data Base on Atlas ///////////////////////////////////////////////////////////////////////////////
 
-
-const imageSchema = {
-    imagestring: {
-        type: String,
-        require: true
+const DBSch = {
+    imageSchema: {
+        imagestring: {
+            type: String,
+            require: true
+        },
+        Name: {
+            type: String,
+            require: true
+        },
+        descriptions: {
+            type: String,
+            require: true
+        },
+        tags: {
+            type: String
+        },
+        date: {
+            day: {
+                type: Number,
+                require: true
+            },
+            Month: {
+                type:Number,
+                require: true
+            },
+            year: {
+                type: Number,
+                require: true
+            }
+        }
     },
-    Name: {
-        type: String,
-        require: true
+    siteSetting: {
+        SiteName: {
+            type : String,
+            require: true
+        },
+        Email: {
+            type: String,
+            requere: true
+        },
+        phone: {
+            type: String,
+        },
+        instaid: {
+            type: String,
+        },
+        facebookid: {
+            type: String
+        },
+        twitterid: {
+            type: String
+        },
+        linkdin: {
+            type: String
+        }
     },
-    descriptions: {
-        type: String,
-        require: true
-    }
-}
-const siteSetting = {
-    SiteName: {
-        type : String,
-        require: true
-    },
-    Email: {
-        type: String,
-        requere: true
-    },
-    phone: {
-        type: String,
-    },
-    instaid: {
-        type: String,
-    },
-    facebookid: {
-        type: String
-    },
-    twitterid: {
-        type: String
-    },
-    linkdin: {
-        type: String
+    tags: {
+        Name: {
+            type: String,
+            require: true
+        }
     }
 }
 
 function connectToAtlas(dataBase) {
-    const uri =  `mongodb+srv://${process.env.MA_username}:${process.env.MA_password}@cluster0.wnnrb.mongodb.net/${dataBase}?retryWrites=true&w=majority`;
+    const uri =  `mongodb+srv://ArtGallery:5CV@jah!r2uwiSZ@cluster0.wnnrb.mongodb.net/${dataBase}?retryWrites=true&w=majority`;
     mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+    funs();
+
 }
 
 // Mongoose Data Base on Atlas END //////////////////////////////////////////////////////////////////////////
@@ -98,8 +127,7 @@ function connectToAtlas(dataBase) {
 
 app.route("/")
     .get((req, res)=> {
-
-        res.sendFile(__dirname+"/src/home.html");
+        res.render("Home");
     });
 
 app.route("/gallery")
@@ -126,8 +154,12 @@ app.route("/getin")
     });
 
 
+let port = process.env.PORT;
 
+if (port == null || port == "") {
+  port = 8000;
+}
 
-app.listen(3000, function() {
+app.listen(port, function() {
     console.log("Server started on port 3000");
   });
